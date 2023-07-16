d64fafa6e27a75bd9e949fd31fac34da2f0cd908
FaithEmberwolf <Roxee.l.leeder@outlook.com>
2023-07-16 20:52:42 +1000

    so fing close

--- Assets/Scripts/PotionMakerScript.cs
+++ Assets/Scripts/PotionMakerScript.cs
@@ -6,86 +6,70 @@ using UnityEngine.UI;
 
 public class PotionMakerScript : MonoBehaviour
 {
-    public GameObject[] correctIngredients;                                             // Array of correct ingredients
-    public GameObject[] wrongIngredients;                                               // Array of wrong ingredients
-    public Transform spawnLocation;                                                     // Reference to spawn location object
-    public string winSceneName = "Win_Scene";                                           // Name of the win scene
-    public string gameOverSceneName = "Game_Over";                                      // Name of the game over scene
-    public int maxWrongClicks = 3;                                                      // Maximum allowed wrong clicks
-    public Slider hpSlider;                                                             // Reference to the hp slider component
-    public float spawnDelay = 1f;                                                       // Delay between each spawn
-    public float clickTimeout = 3f;                                                     // Time allowed to click an object
+    public GameObject[] correctIngredients;                                                                                         //Array of correct ingredients
+    public GameObject[] wrongIngredients;                                                                                           //Array of wrong ingredients
+    public Transform spawnLocation;                                                                                                 //Reference to spawn location object
+    public string winSceneName = "Win_Scene";                                                                                       //Name of the win scene
+    public string gameOverSceneName = "Game_Over";                                                                                  //Name of the game over scene
+    public int maxWrongClicks = 3;                                                                                                  //Maximum allowed wrong clicks
+    public Slider hpSlider;                                                                                                         //Reference to the hp slider component
+    public float spawnDelay = 1f;                                                                                                   //Delay between each spawn
+    public float clickTimeout = 3f;                                                                                                 //Time allowed to click an object
+    public Vector3 ingredientScale = new Vector3(2f, 2f, 2f);                                                                       //Adjust the scale values as per your requirement
+    
 
-    private int clickedCorrectIngredients = 0;                                          // Counter for clicked correct ingredients
-    private int wrongClicks = 0;                                                        // Counter for wrong clicks
-    private bool canAppearAgain = true;                                                 // Flag to control appearance of incorrect ingredients
+    private int clickedCorrectIngredients = 0;                                                                                      //Counter for clicked correct ingredients
+    private int wrongClicks = 0;                                                                                                    //Counter for wrong clicks
+    private bool canAppearAgain = true;                                                                                             //Flag to control reappearance of ingredients
 
     private void Start()
     {
-        hpSlider.maxValue = maxWrongClicks;                                             // Set the maximum value of the slider to the maximum wrong clicks
-        hpSlider.value = 0;                                                             // Initialize the slider value to 0
-        StartCoroutine(SpawnObjects());                                                 // Start spawning the ingredients
+        hpSlider.maxValue = maxWrongClicks;                                                                                         //Set the maximum value of the slider to the maximum wrong clicks
+        hpSlider.value = 0;                                                                                                         //Initialize the slider value to 0
+        StartCoroutine(SpawnObjects());                                                                                             //Start spawning the ingredients
     }
 
     private IEnumerator SpawnObjects()
     {
-        List<GameObject> allIngredients = new List<GameObject>();
+        
+            List<GameObject> allIngredients = new List<GameObject>();                                                                   //Create a list to store all the ingredients
 
-        //Combine correct and wrong ingredients into a single list
+            allIngredients.AddRange(correctIngredients);                                                                                //Add the correct ingredients to the list
+            allIngredients.AddRange(wrongIngredients);                                                                                  //Add the wrong ingredients to the list
 
-        allIngredients.AddRange(correctIngredients);
-        allIngredients.AddRange(wrongIngredients);
+            ShuffleList(allIngredients);                                                                                                //Shuffle the list of ingredients
 
-        //Shuffle the list of ingredients
-
-        ShuffleList(allIngredients);
-
-        foreach (GameObject ingredient in allIngredients)
-        {
-            //Instantiate the ingredient at the spawn position
-
-            GameObject spawnedIngredient = Instantiate(ingredient, GetRandomPositionInArea(), Quaternion.identity);
-
-            if (ingredient.CompareTag("Correct_Ingredient"))
+            foreach (GameObject ingredient in allIngredients)
             {
-                //Attach the ClickableObject script to the spawned correct ingredient
+                GameObject spawnedIngredient = Instantiate(ingredient, GetRandomPositionInArea(), Quaternion.identity);                 //Instantiate the ingredient at the spawn position
 
-                ClickableObject clickableObject = spawnedIngredient.AddComponent<ClickableObject>();
-                clickableObject.canAppearAgain = canAppearAgain;
+                spawnedIngredient.transform.localScale = ingredientScale;                                                               //Set the scale of the object
+
+                //if (ingredient.CompareTag("Correct_Ingredient"))
+                //{
+                StartCoroutine(OnMouseDown());                                                                                      //Start OnMouseDown coroutine
+
+                //}
+                //else if (ingredient.CompareTag("Incorrect_Ingredient"))
+                //{
+                StartCoroutine(TimeoutObject(spawnedIngredient));                                                                   //Start the timeout coroutine for incorrect ingredients, passing the spawned ingredient as an argument
+                                                                                                                                    //}
+
+                yield return new WaitForSeconds(spawnDelay);                                                                            //Wait for a delay before spawning the next ingredient
             }
-            else if (ingredient.CompareTag("Incorrect_Ingredient"))
-            {
-                StartCoroutine(TimeoutObject(spawnedIngredient));                           //Start the timeout coroutine for incorrect ingredients
-            }
-
-            //Wait for a delay before spawning the next ingredient
-
-            yield return new WaitForSeconds(spawnDelay);
-        }
+            yield return new WaitForSeconds(clickTimeout);                                                                          //Wait for the specified timeout duration
+               
     }
+
 
     private IEnumerator TimeoutObject(GameObject obj)
     {
-        yield return new WaitForSeconds(clickTimeout);
-
-        //Check if the object is still active (not clicked) after the timeout
-
-        if (obj && obj.activeSelf)
+        while (canAppearAgain)
         {
-           
+            yield return new WaitForSeconds(clickTimeout);
 
-            wrongClicks++;
-            hpSlider.value = wrongClicks;
-
-            if (wrongClicks > maxWrongClicks)
+            if (obj && obj.activeSelf)                                                                                                   //Check if the object is still active (not clicked) after the timeout
             {
-                SceneManager.LoadScene(gameOverSceneName);                                  //Load the game over scene if maximum wrong clicks reached
-
-                Debug.Log("Nuh you can't game over");
-            }
-            else
-            {
-                //Deactivate it and wait for the spawnDelay to reactivate it
 
                 obj.SetActive(false);
                 yield return new WaitForSeconds(spawnDelay);
@@ -94,20 +78,21 @@ public class PotionMakerScript : MonoBehaviour
         }
     }
 
+
     private Vector3 GetRandomPositionInArea()
     {
-        Vector3 areaCenter = spawnLocation.position;                                         //Get the position of the spawn location object
-        Vector3 areaSize = spawnLocation.localScale;                                         //Get the scale of the spawn location object
+        Vector3 areaCenter = spawnLocation.position;                                                                                //Get the position of the spawn location object
+        Vector3 areaSize = spawnLocation.localScale;                                                                                //Get the scale of the spawn location object
 
-        Vector3 minPosition = areaCenter - areaSize / 2f;                                    //Calculate the minimum position within the spawn area
-        Vector3 maxPosition = areaCenter + areaSize / 2f;                                    //Calculate the maximum position within the spawn area
+        Vector3 minPosition = areaCenter - areaSize / 2f;                                                                           //Calculate the minimum position within the spawn area
+        Vector3 maxPosition = areaCenter + areaSize / 2f;                                                                           //Calculate the maximum position within the spawn area
 
         Vector3 randomPosition = new Vector3(
-            Random.Range(minPosition.x, maxPosition.x),                                      //Get a random x position within the area bounds
-            Random.Range(minPosition.y, maxPosition.y),                                      //Get a random y position within the area bounds
-            Random.Range(minPosition.z, maxPosition.z));                                     //Get a random z position within the area bounds
+            Random.Range(minPosition.x, maxPosition.x),                                                                             //Get a random x position within the area bounds
+            Random.Range(minPosition.y, maxPosition.y),                                                                             //Get a random y position within the area bounds
+            Random.Range(minPosition.z, maxPosition.z));                                                                            //Get a random z position within the area bounds
 
-        return randomPosition;                                                               //Return the random position within the area
+        return randomPosition;                                                                                                      //Return the random position within the area
     }
 
     private void Update()
@@ -127,7 +112,7 @@ public class PotionMakerScript : MonoBehaviour
 
                     if (clickedCorrectIngredients == correctIngredients.Length)
                     {
-                        SceneManager.LoadScene(winSceneName);                               //Load the win scene if all correct ingredients have been clicked
+                        SceneManager.LoadScene(winSceneName);                                                                       //Load the win scene if all correct ingredients have been clicked
                     }
 
                     Destroy(hitObject);
@@ -138,31 +123,25 @@ public class PotionMakerScript : MonoBehaviour
 
                     if (wrongClicks > maxWrongClicks)
                     {
-                        SceneManager.LoadScene(gameOverSceneName);                          //Load the game over scene if maximum wrong clicks reached
+                        SceneManager.LoadScene(gameOverSceneName);                                                                  //Load the game over scene if maximum wrong clicks reached
                     }
-                   
-                    //Deactivate the incorrect ingredient object instead of destroying it
 
-                    hitObject.SetActive(false);
+                    hitObject.SetActive(false);                                                                                     //Deactivate the incorrect ingredient object instead of destroying it
                 }
 
-                //Update the slider value based on the number of wrong clicks
-
-                hpSlider.value = wrongClicks;
+                hpSlider.value = wrongClicks;                                                                                       //Update the slider value based on the number of wrong clicks
             }
         }
     }
 
     private void ShuffleList<T>(List<T> list)
     {
-        int n = list.Count;                                                                  //Get the total number of elements in the list
+        int n = list.Count;                                                                                                         //Get the total number of elements in the list
 
-        //Loop through the list in reverse order
-
-        while (n > 1)
+        while (n > 1)                                                                                                               //Loop through the list in reverse order
         {
-            n--;                                                                            //Decrement the index to get the current position
-            int k = Random.Range(0, n + 1);                                                 //Generate a random index between 0 and n (inclusive)
+            n--;                                                                                                                    //Decrement the index to get the current position
+            int k = Random.Range(0, n + 1);                                                                                         //Generate a random index between 0 and n (inclusive)
 
             //Swap the element at index k with the element at index n
 
@@ -172,29 +151,25 @@ public class PotionMakerScript : MonoBehaviour
         }
     }
 
-    public class ClickableObject : MonoBehaviour
+
+    public IEnumerator OnMouseDown()
     {
-       public bool canAppearAgain;                                                     //Flag to control appearance of Incorrect Ingredients
-
-
-        private void OnMouseDown()
+        if (gameObject.CompareTag("Correct_Ingredient"))
         {
-            if (gameObject.CompareTag("Correct_Ingredient"))
+            if (!canAppearAgain)
             {
-                if (!canAppearAgain)
-                {
-                    //Object with the tag "Correct_Ingredient" was already clicked and should not appear again
-                    gameObject.SetActive(false);
-                }
-                else
-                {
-                    canAppearAgain = false;
-                }
+                gameObject.SetActive(false);                                                                                        //Deactivate the game object if it has the tag "Correct_Ingredient" and should not appear again
             }
-            else if (gameObject.CompareTag("Incorrect_Ingredient"))
+            else
             {
-                // Object with the tag "Incorrect_Ingredient" was clicked, so it can continue to appear
+                canAppearAgain = true;                                                                                              //Set the canAppearAgain flag to true if the game object can appear again
             }
         }
+        else if (gameObject.CompareTag("Incorrect_Ingredient"))
+        {
+            canAppearAgain = true;                                                                                                  //Set the canAppearAgain flag to true if the game object has the tag "Incorrect_Ingredient" and can continue to appear
+        }
+
+        yield return new WaitForSeconds(clickTimeout);                                                                              // Wait for the specified timeout duration
     }
 }
