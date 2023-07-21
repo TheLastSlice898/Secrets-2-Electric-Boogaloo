aefe17c62e368483ef686c6db275eda26b128ece
CamStar28 <129808323+CamStar28@users.noreply.github.com>
2023-07-21 16:47:42 +1000

    Comments
    Be sure to like, comment, and Subscribe, turn on notifications so you never miss an upload, follow me on twitter, instagram, MySpace, facebook, reddit, twitter, onlyfans, twitter

--- Assets/Scripts/CatchThe Book Scripts/CatchTheBook.cs
+++ Assets/Scripts/CatchThe Book Scripts/CatchTheBook.cs
@@ -8,34 +8,41 @@ public class CatchTheBook : MonoBehaviour
 {
     public TextMeshProUGUI healthBox;
     int bookHealth; 
+    //variable for the health of the book, which relates to our win condition, as well as a UI element link so we can display it to the player
 
-    public float secondCounter; 
-    public int FlyTime; 
-    public bool BookIsFlying;
+    public float secondCounter; //keeps track of the number of seconds that has passed. Resets upon certain conditions
+    public int FlyTime; //A variable to hold the random time that the book will fly
+    public bool BookIsFlying; //a bool so we can check whether or not the book is currently flying
 
-    public bool BookOnLeft;
-    public bool BookOnRight;
+    public bool BookOnLeft; //let's us check that the book is on the left
+    public bool BookOnRight; //let's us check that the book is on the right 
 
     public TextMeshProUGUI timeBox; 
     public float loseTimer; 
+    //a seperate, overall timer that does not reset, tied to the lose condition 
 
     // Start is called before the first frame update
     void Start()
     {
         bookHealth = 3; 
         healthBox.text = "Book HP: " + bookHealth; 
+        //Ensures the Book's health is reset when the scene loads, as well as updating the UI
         
         BookOnLeft = true; 
         BookOnRight = false; 
         transform.position = new Vector3(-5.5f, 0.87f, -7.2f); 
+        //resets the book's position to the left, sets variables to reflect this
 
         secondCounter = 0.0f;
         BookIsFlying = false; 
+        //resets more variables
 
         loseTimer = 30f; 
         timeBox.text = "Time Remaining: " + loseTimer; 
+        //resets the lose timer
 
         RandomiseFlyTime(); 
+        //triggers the function to randomise the time that the book flies 
     }
 
     // Update is called once per frame
@@ -46,19 +53,23 @@ public class CatchTheBook : MonoBehaviour
         //Debug.Log(secondCounter);
         //Debug.Log(FlyTime); 
         timeBox.text = "Time Remaining: " + (int)loseTimer; 
+        //increases the secondCounter while decreasing the loseTimer
 
         if(secondCounter >= FlyTime && BookIsFlying == false) {
             BookIsFlying = true;
             //Debug.Log("Fly you fool"); 
         }
+        //sets the variable to allow the player to catch the book
 
+        //the behaviour for when the player catches the book
         if(Input.GetKeyDown("space") && BookIsFlying == true) {
-            bookHealth -= 1; 
-            healthBox.text = "Book HP: " + bookHealth; 
+            bookHealth -= 1; //lowers the health
+            healthBox.text = "Book HP: " + bookHealth; //updates the UI
              
-            secondCounter = 0f; 
+            secondCounter = 0f; //resets the secondCounter
             //FlyTime = 100; 
 
+            //if statement to swap the onLeft and onRight variables to relfect which is true, so certain functions fire correctly 
             if(BookOnLeft == true) { 
                 MoveBookRight();
                 } 
@@ -66,26 +77,30 @@ public class CatchTheBook : MonoBehaviour
                      MoveBookLeft();
                     }
 
-            RandomiseFlyTime(); 
-            BookIsFlying = false;
+            RandomiseFlyTime(); //get a new random fly time
+            BookIsFlying = false; //ensures the player cannot catch the book multiple times at once 
         }
+        
 
         if(BookIsFlying == true && BookOnLeft == true) {
             transform.position += new Vector3(0,0,30) * Time.deltaTime;
         }
+        //moves the book right
 
         if(BookIsFlying == true && BookOnRight == true) {
             transform.position += new Vector3(0,0,-30) * Time.deltaTime;
         }
+        //moves the book left
 
         if(loseTimer <= 0f) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
         }
+        //reset the scene upon a loss
     }
 
     public void RandomiseFlyTime()
     {
-        FlyTime = Random.Range(1,6); 
+        FlyTime = Random.Range(1,6); //randomises the amount of seconds before the book flies
     }
 
     public void MoveBookRight()
@@ -95,6 +110,7 @@ public class CatchTheBook : MonoBehaviour
         BookOnLeft = false;
         transform.position = new Vector3(-5.5f, 0.87f, 7.2f);
     }
+    //function to be activated by a trigger
 
     public void MoveBookLeft()
     {
@@ -103,4 +119,5 @@ public class CatchTheBook : MonoBehaviour
         BookOnLeft = true; 
         transform.position = new Vector3(-5.5f, 0.87f, -7.2f); 
     }
+    //function to be activated by a trigger
 }
