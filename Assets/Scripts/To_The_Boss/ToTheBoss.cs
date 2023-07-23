d3654d32a6646d97e9b0d148b25d3a3c6e745fb0
FaithEmberwolf <Roxee.l.leeder@outlook.com>
2023-07-23 16:29:48 +1000

    works

--- Assets/Scripts/To_The_Boss/ToTheBoss.cs
+++ Assets/Scripts/To_The_Boss/ToTheBoss.cs
@@ -6,179 +6,111 @@ using UnityEngine.UI;
 
 public class ToTheBoss : MonoBehaviour
 {
+    public GameObject pileOfLogs;
+    public GameObject magicBarricade;
+    public Transform spawnLocation;                                                                             //Specific location for objects to spawn
+    public int correctPressesNeeded = 5;                                                                        //Number of correct presses needed to win
+    public int incorrectPressesAllowed = 3;                                                                     //Number of incorrect presses allowed before losing
+    public string winSceneName = "Win_Scene";                                                                   //Name of the win scene
+    public string gameOverSceneName = "Game_Over";                                                              //Name of the game over scene
+    public Text correctPressesText;                                                                             //Reference to the UI Text element to display correct presses
 
-    public GameObject[] pileofLogs;
-    public GameObject[] magicBlockade;
-    public Transform spawnLocation;
-    public Slider hpSlider;
-    public Text correctChoicesCounter; 
-    public string winSceneName = "Win_Scene";
-    public string gameOverSceneName = "Game_Over";
-    public int maxWrongPress = 3;
 
-    public int correctSelection = 0;
-    public float spawnDelay = 2f;                                                                                                   //Delay between each spawn
-    //public float pressTimeout = 3f;                                                                                                 //Time allowed to click an object
-    public float deactivateDuration = 1f;
-    public float wrongPress = 0;
-    public Vector3 ingredientScale = new Vector3(2f, 2f, 2f);                                                                       //Adjust the scale values as per your requirement
+    private GameObject currentObject;
+    private int correctPresses = 0;
+    private int incorrectPresses = 0;
+    private bool canPress = true;                                                                               //To prevent button presses during the delay
 
-    private GameObject currentlyActiveObject;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        hpSlider.maxValue = maxWrongPress;
-        hpSlider.value = 0;
-
-        correctChoicesCounter.text = correctSelection.ToString();
-
+        StartCoroutine(ShowObjectsWithDelay());
     }
 
-    private IEnumerator SpawnHazard()
+    private IEnumerator ShowObjectsWithDelay()
     {
-            List<GameObject> allHazards = new List<GameObject>();
-
-            allHazards.AddRange(pileofLogs);
-            allHazards.AddRange(magicBlockade);
-
-            ShuffleList(allHazards);
-
-            foreach (GameObject hazard in allHazards)
-            {
-
-                if (hazard != null) // Deactivate the previously active hazard
-                {
-                    if (currentlyActiveObject != null)
-                    {
-                        currentlyActiveObject.SetActive(false);
-                    }
-
-                    hazard.SetActive(true); // Activate the new hazard
-
-                    currentlyActiveObject = hazard; // Update the currently active hazard reference
-
-                    yield return new WaitForSeconds(spawnDelay);
-                }
-
-            }
-    }
-
-    private void ShuffleList<T>(List<T> list)
-    {
-        int n = list.Count;                                                                                                         //Get the total number of elements in the list
-
-        while (n > 1)                                                                                                               //Loop through the list in reverse order
+        while (correctPresses < correctPressesNeeded && incorrectPresses < incorrectPressesAllowed)
         {
-            n--;                                                                                                                    //Decrement the index to get the current position
-            int k = Random.Range(0, n + 1);                                                                                         //Generate a random index between 0 and n (inclusive)
+            yield return new WaitForSeconds(2f);
 
-            //Swap the element at index k with the element at index n
-
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
-        }
-    }
-
-    private IEnumerator TimeoutObject(GameObject obj)
-    {
-        //yield return new WaitForSeconds(pressTimeout);
-
-            if (obj && obj.activeSelf)                                                                                                   //Check if the object is still active (not clicked) after the timeout
+            if (correctPresses < correctPressesNeeded && incorrectPresses < incorrectPressesAllowed)
             {
-                obj.SetActive(false);
-                yield return new WaitForSeconds(spawnDelay);
-                obj.SetActive(true);
-            }
-        
-    }
-
-    private IEnumerator DeactivateAndReactivate(GameObject obj)
-    {
-        obj.SetActive(false);
-        yield return new WaitForSeconds(deactivateDuration);
-        obj.SetActive(true);
-
-    }
-    
-    private Vector3 GetRandomPositionInArea()
-    {
-            Vector3 areaCenter = spawnLocation.position;                                                                                //Get the position of the spawn location object
-            Vector3 areaSize = spawnLocation.localScale;                                                                                //Get the scale of the spawn location object
-
-            Vector3 minPosition = areaCenter - areaSize / 2f;                                                                           //Calculate the minimum position within the spawn area
-            Vector3 maxPosition = areaCenter + areaSize / 2f;                                                                           //Calculate the maximum position within the spawn area
-
-            Vector3 randomPosition = new Vector3(
-            Random.Range(minPosition.x, maxPosition.x),                                                                                 //Get a random x position within the area bounds
-            Random.Range(minPosition.y, maxPosition.y),                                                                                 //Get a random y position within the area bounds
-            Random.Range(minPosition.z, maxPosition.z));                                                                                //Get a random z position within the area bounds
-
-        return randomPosition;                                                                                                          //Return the random position within the area
-    
-    }
-
-    private void IncrementCounter()
-    {
-        correctSelection++;
-        correctChoicesCounter.text = correctSelection.ToString();
-    }
-
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (currentlyActiveObject != null && currentlyActiveObject.CompareTag("PileOfLogs"))
-            {
-                correctSelection++;
-
-                if (correctSelection == 5)
-                {
-                    SceneManager.LoadScene(winSceneName);
-                }
-
-                currentlyActiveObject.SetActive(false);
-                StartCoroutine(SpawnHazard());
-            }
-            else
-            {
-                wrongPress++;
-
-                if (wrongPress > maxWrongPress)
-                {
-                    SceneManager.LoadScene(gameOverSceneName);
-                }
+                ShowRandomObject();
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (correctPresses >= correctPressesNeeded)
         {
-            if (currentlyActiveObject != null && currentlyActiveObject.CompareTag("MagicBlockade"))
-            {
-                correctSelection++;
+            //Game over, player won!
+            Debug.Log("You win!");
+            SceneManager.LoadScene(winSceneName);                                                               //Load the win scene if all correct ingredients have been clicked
+        }
+        else
+        {
+            //Game over, player lost!
+            Debug.Log("Game over. You lose!");
+            SceneManager.LoadScene(gameOverSceneName);                                                          //Load the game over scene if maximum wrong clicks reached
+        }
+    }
 
-                if (correctSelection == 5)
-                {
-                    SceneManager.LoadScene(winSceneName);
-                }
-
-                currentlyActiveObject.SetActive(false);
-                StartCoroutine(SpawnHazard());
-            }
-            else
-            {
-                wrongPress++;
-
-                if (wrongPress > maxWrongPress)
-                {
-                    SceneManager.LoadScene(gameOverSceneName);
-                }
-            }
+    private void ShowRandomObject()
+    {
+        if (Random.Range(0, 2) == 0)
+        {
+            currentObject = pileOfLogs;
+        }
+        else
+        {
+            currentObject = magicBarricade;
         }
 
-        hpSlider.value = wrongPress;
+        currentObject.transform.position = spawnLocation.position;                                              //Set the position of the current object to the spawn location
+        currentObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (canPress)
+        {
+            if (Input.GetKeyDown(KeyCode.A) && currentObject == pileOfLogs)
+            {
+                CorrectPress();
+            }
+            else if (Input.GetKeyDown(KeyCode.D) && currentObject == magicBarricade)
+            {
+                CorrectPress();
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                IncorrectPress();
+            }
+        }
+    }
+
+    private void CorrectPress()
+    {
+        canPress = false;                                                                                       //Disable button presses during delay
+        currentObject.SetActive(false);
+
+        correctPresses++;
+
+        StartCoroutine(EnableButtonPress());
+    }
+
+    private void IncorrectPress()
+    {
+        canPress = false;                                                                                       //Disable button presses during delay
+        currentObject.SetActive(false);
+
+        incorrectPresses++;
+
+        StartCoroutine(EnableButtonPress());
+    }
+
+    private IEnumerator EnableButtonPress()
+    {
+        yield return new WaitForSeconds(2f);
+        canPress = true;                                                                                         //Enable button presses after the delay
+        ShowObjectsWithDelay();
     }
 }
 
