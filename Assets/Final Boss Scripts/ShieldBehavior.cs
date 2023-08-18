cdc0d3b4c6b7d06b4df93bf1314d61c5f28a5381
CamStar28 <129808323+CamStar28@users.noreply.github.com>
2023-08-18 16:05:50 +1000

    Added new Assets

--- Assets/Final Boss Scripts/ShieldBehavior.cs
+++ Assets/Final Boss Scripts/ShieldBehavior.cs
@@ -7,31 +7,31 @@ public class ShieldBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(-1, 6, 2); 
-        transform.eulerAngles = new Vector3(180,0,0);
+        transform.position = new Vector3(-1, 6, 2.5f); 
+        transform.eulerAngles = new Vector3(90,270,0);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.position = new Vector3(-1, 6, 2); 
-            transform.eulerAngles = new Vector3(180,0,0);
+            transform.position = new Vector3(-1, 6, 2.5f); 
+            transform.eulerAngles = new Vector3(90,270,0);
         }
 
         if(Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.DownArrow)) {
-            transform.position = new Vector3(-1, 6, -1.5f); 
-            transform.eulerAngles = new Vector3(0,0,0);
+            transform.position = new Vector3(-1, 6, -2); 
+            transform.eulerAngles = new Vector3(90,-270,0);
         }
 
         if(Input.GetKeyDown("d") || Input.GetKeyDown(KeyCode.RightArrow)) {
-            transform.position = new Vector3(1, 6, 0.4f); 
-            transform.eulerAngles = new Vector3(0,-90,0);
+            transform.position = new Vector3(1.5f, 6, 0.4f); 
+            transform.eulerAngles = new Vector3(90,0,0);
         }
         
         if(Input.GetKeyDown("a") || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            transform.position = new Vector3(-3, 6, 0.4f); 
-            transform.eulerAngles = new Vector3(0,90,0);
+            transform.position = new Vector3(-3.5f, 6, 0.4f); 
+            transform.eulerAngles = new Vector3(90,180,0);
         }
     }
 
